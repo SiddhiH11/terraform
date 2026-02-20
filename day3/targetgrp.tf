c1832f536463e3f81aef250a889080b8a5e1c14f
SiddhiH-11 <siddhi@twjit.in>
2026-02-21 00:53:56 +0530

    fix

--- day3/targetgrp.tf
+++ day3/targetgrp.tf
@@ -2,6 +2,7 @@ resource "aws_lb_target_group" "home_target_group" {
     name = "${var.project}-${var.env}-home-tg"
     port = 80
     protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
     health_check {
         enabled = true
         path = "/"
@@ -14,6 +15,7 @@ resource "aws_lb_target_group" "mobile_target_group" {
     name = "${var.project}-${var.env}-mobile-tg"
     port = 80
     protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
     health_check {
         enabled = true
         path = "/mobile/"
@@ -26,6 +28,7 @@ resource "aws_lb_target_group" "laptop_target_group" {
     name = "${var.project}-${var.env}-laptop-tg"
     port = 80
     protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
     health_check {
         enabled = true
         path = "/laptop/"
